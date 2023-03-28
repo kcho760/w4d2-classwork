15d8f2c6ff83969d21b2fd4b735f743d9e5910d0
kcho760 <kcho760@gmail.com>
2023-03-28 16:26:45 -0400

    Added piece and board move_piece

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -1,22 +1,43 @@
+require_relative "piece"
+
 class Board
 
-    def initialize
-        @grid = Array.new(8) { Array.new(8, Piece.new(color, board, pos)) }
 
+    def initialize
+        @rows = Array.new(8) { Array.new(8, Piece.new(color, board, pos))}
+
+        # @null_piece = Null_piece.new()
     end
 
     def [](position)
         row , col = position 
-        @grid[row][col]
+        @rows[row][col]
     end 
 
     def []=(position,value)
         row, col = position 
-        @grid[row][col] = value 
+        @rows[row][col] = value 
     end 
 
-    def fill_grid 
-        @grid
+    def move_piece(start_pos,end_pos)
+        if self[start_pos].nil?
+            raise "there is no piece"
+        end
+        if !self[end_pos].nil?
+            raise "there is a piece there"
+        end
+    
+        # self[start_pos] = nil
+        # self[end_pos] = start
+        
+
+        self[start_pos],self[end_pos] = self[end_pos],self[start_pos]
+    end
+
+
+
+    def fill_rows 
+        @rows
 
     end 
 end 
